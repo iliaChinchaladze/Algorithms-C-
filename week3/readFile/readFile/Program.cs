86d8e2c10129e7b94ff712c577b0bffae6551d07
iliaChinchaladze <79948454+iliaChinchaladze@users.noreply.github.com>
2022-02-23 16:18:59 +0000

    Add files via upload

---
+++ week3/readFile/readFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace readFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            readFile("textFile.txt");
+        }
+        static void readFile(string textFile)
+        {
+            const int MAX_FILE_LINES = 50000;
+            string[] AllLines = new string[MAX_FILE_LINES];
+
+            //reads from bin/DEBUG subdirectory of project directory
+            AllLines = File.ReadAllLines(textFile);
+
+
+            foreach (string line in AllLines)
+            {
+                //split words using space , . ?
+                string[] words = line.Split(' ', ',', '.', '?', ';', ':', '!');
+                foreach (string word in words)
+                    if (word != "")
+                    {
+                        Console.Write("" + word.ToLower());
+   
+                    }
+
+            }
+
+        }
+    }
+}
